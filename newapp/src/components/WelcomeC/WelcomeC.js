74f14150a3e57dd214c55b0be43ddc1d225fe494
Mikhail Nosov <manosov1984@gmail.com>
2021-12-28 23:45:19 +0400

    modify class

--- newapp/src/components/WelcomeC/WelcomeC.js
+++ newapp/src/components/WelcomeC/WelcomeC.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-
-
 export default class WelcomeC extends React.Component {
   render() {
     if (this.props.user) {
       return <div>Class Hello, {this.props.user}</div>
-    }
-    return <h1>Class Hello, Unknown.</h1>;
+    // }
+    // return <h1>Class Hello, Unknown.</h1>;
+  }
   }
 }
+
+WelcomeC.defaultProps = { user: "Unknown" }
 
